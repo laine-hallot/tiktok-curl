35330ec22f5f99d2e8cbd8f69ecb7ac427679f92
Laine Hallot <lainehallot@gmail.com>
2023-05-31 17:21:25 -0500

    removed refrences to uninstalled packages

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * @format
  */
 
-import 'node-libs-react-native/globals';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,4 @@
-const nodelibs = require('node-libs-react-native');
-nodelibs.vm = require.resolve('vm-browserify');
-
 module.exports = {
-  resolver: {
-    extraNodeModules: nodelibs,
-  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
